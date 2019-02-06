e78124df973658c08ce90bd9a0561ea9915f8b31
Ana Helena <anahelenarp@hotmail.com>
2019-02-06 13:13:11 -0200

    add animal.ts

---
+++ animal.ts
@@ -0,0 +1,13 @@
+import { create } from "domain";
+
+export class Animal { 
+  constructor(public name: string, public size: number) { }
+}
+
+let animal: Animal;
+
+function createAnimal(name: string, size: number): Animal {
+  return new Animal(name, size);
+}
+
+animal = createAnimal('Canine', 500);
